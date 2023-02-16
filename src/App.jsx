8485170574bd51767e4e4fd501fb0e4a1c9c2dd7
emmacodes <binma.emma@gmail.com>
2023-02-15 23:16:04 -0800

    added word count func

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
+/**
+ * when start game btn is clicked:
+ * 1. Time remaining start to countdown
+ * 2. when time remaining is 0, word count displayed the how many words 
+ * 
+ */
 import { useState } from 'react'
-
 import './App.css'
 
 function App() {
   const [text, setText] = useState('')
+  const [timeRemaining, setTimeRemaining] = useState(10)
+
   function handleChange(e){
     setText(e.target.value)
+  }
+  
+  // 1. need to trim the empty space after the last word
+  // 2. only count the word that is not empty space using filter
+  function wordCount(){
+    const wordArr = text.split(' ')
+    let total = 0
+    for (let word in wordArr){
+      total ++
+    }
+    console.log(total)
+    return  total
+
   }
 
   return (
@@ -18,8 +38,8 @@ function App() {
         value={text}/>
     
       
-      <h4>Time remaining:</h4>
-      <button>Start Game</button>
+      <h4>Time remaining:{timeRemaining}</h4>
+      <button onClick = {wordCount}>Start Game</button>
       <h1>Word Count:??</h1>
 
  
